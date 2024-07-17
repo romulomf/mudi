f83e1365c0c203e726a00048a2454ea03b96b46d
Rômulo Machado Flores <romuloflores@gmail.com>
2024-07-16 21:44:30 -0300

    Atualização do SpringBoot para a v2.7.18
    Correção do tipo do ID do UserRepository.
    Adicionado o namespace do thymeleaf para habilitar suporte do editor no arquivo base.html.

--- src/main/java/br/com/alura/mvc/mudi/repository/UserRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.mvc.mudi.model.User;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 	User findByUsername(String username);
 }
