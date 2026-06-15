111ec0830bbf7dde24cf8d7b010d1c9a97b0dfd9
Rômulo Machado Flores <romuloflores@gmail.com>
2026-06-15 19:32:23 -0300

    Atualização SpringBoot v4.1
    Alteração no filtro e ordenação nas consultas da Spring Data JPA, que agora usam um estilo type-safe e mais amigável para a refatoração.

--- src/main/java/br/com/alura/mvc/mudi/api/PedidosRest.java
+++ src/main/java/br/com/alura/mvc/mudi/api/PedidosRest.java
@@ -2,7 +2,6 @@ package br.com.alura.mvc.mudi.api;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,20 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/pedidos")
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class PedidosRest {
 
-	@Autowired
-	private PedidoRepository pedidoRepository;
+	private final PedidoRepository pedidoRepository;
 
 	@GetMapping("aguardando")
 	public List<Pedido> getPedidosAguardandoOfertas() {
-		Sort ordenacao = Sort.by("id").descending();
-		PageRequest criterio = PageRequest.of(0, 10, ordenacao);
+		PageRequest criterio = PageRequest.of(0, 10, Sort.by(Pedido::getId).descending());
 		return pedidoRepository.findByStatus(StatusPedido.AGUARDANDO, criterio);
 	}
 }
--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,7 +3,6 @@ package br.com.alura.mvc.mudi.controller;
 import java.security.Principal;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
@@ -15,24 +14,19 @@ import org.springframework.web.context.annotation.RequestScope;
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestScope
 @RequestMapping("/home")
+@RequiredArgsConstructor
 public class HomeController {
 
-	@Autowired
-	private PedidoRepository pedidoRepository;
-
-	public HomeController() {
-		// construtor padrão
-	}
+	private final PedidoRepository pedidoRepository;
 
 	@GetMapping
 	public String home(Model model, Principal principal) {
-		
-		Sort ordenacao = Sort.by("dataDeEntrega").descending();
-		PageRequest criterio = PageRequest.of(0, 1, ordenacao);
+		PageRequest criterio = PageRequest.of(0, 1, Sort.by(Pedido::getDataDeEntrega).descending());
 		
 		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, criterio);
 		model.addAttribute("pedidos", pedidos);
